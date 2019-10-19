5b7a60ebca68852efb88399055aee51adbb5abcf
Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-10-19 23:36:53 +0600

    database configuration updated

--- django_app_deploy_on_vps/settings.py
+++ django_app_deploy_on_vps/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_app_deploy_on_vps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_deploy',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
